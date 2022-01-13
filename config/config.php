fce790c0385d88c18046633c5fc573b0ba9400d7
Mano200600700 <88878047+Mano200600700@users.noreply.github.com>
2022-01-13 14:38:14 +0200

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php  
+// ================================================================= //
+// ================================================================= //
+// =============== By Mohammed Cha : Re-skinning GRP =============== //
+// ================================================================= //
+// ================================================================= //
+
+$uri = 'https://hothub-tkk.web.app';
+
+$wname = 'HotHub; 
+
+$descrip = 'HotHub Movies Watch Movies Now For Free';  
+
+$cpa_link = 'https://cuworld.cf/82f8fff';  
+
+$ImdbApi = 'c0d40071e081955c2d625f2a44805b63';
+
+$comingsoon = '1';  // 1 to display coming soon or 0 to hide 
+$related = '1'; // 1 to display related movies or 0 to hide 
+
+?>
